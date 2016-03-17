405d6b224391ac0cd0373e9b8944cc3b4c8a5257
Howard Smith <howardsmithnz@gmail.com>
2016-03-17 16:00:20 +1300

    add radio buttons to Moodometer test component

--- src/components/Moodometer.jsx
+++ src/components/Moodometer.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 module.exports = React.createClass({
   render:   function () {
+    let msg = this.props.msg
     return (
-      <div>Moodometer</div>
+      <form action="">
+        <input type="radio" name="mood" value="1"> 1 
+        <input type="radio" name="mood" value="2"> 2 
+        <input type="radio" name="mood" value="3"> 3 
+        <input type="radio" name="mood" value="4"> 4 
+        <input type="radio" name="mood" value="5"> 5 
+      </form>
+      <div>Moodometer says { msg }</div>
     )
   }
 }
