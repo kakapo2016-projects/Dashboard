d9174c26e34eba5107c2453844782beb0b1d75f9
Ashley <Ashleystanbridge@icloul.com>
2016-03-17 19:19:51 +1300

    modified clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
 
 import React from 'react'
-// import Clock from './Clock'
+import Clock from './Clock'
 
 export default React.createClass({
   render() {
     return(
       <div id='app'>
         <h2>THIS IS A PASSING TEST</h2>
-
-
+        <Clock />
       </div>)
   }
 })
