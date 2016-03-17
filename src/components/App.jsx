ea4ba2f77147179aaa63e9ff95febe8328cc074d
Ashley <Ashleystanbridge@icloul.com>
2016-03-18 08:33:33 +1300

    Added bootstraps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,18 @@ import Intention from './Intention.jsx'
 export default React.createClass({
   render() {
     return(
-      <div id='app' className="row">
-        <div className="header">
-          <h1 className="header col-md-12">THIS IS A PASSING TEST</h1>
-        </div>
-        <div className="col-md-6 col-md-offset-3">
-          <Intention />
-          <Clock />
-          <Search />
-        </div>
-      </div>)
+<div id='app' className="row">
+  <div className="col-md-12 head">
+    <div class="page-header">
+      <h1>Dashr <small>Personal dashboard</small></h1>
+    </div>
+  </div>
+  <div className="col-md-6 col-md-offset-3">
+    <Welcome />
+    <Intention />
+    <Clock />
+    <Search />
+  </div>
+</div>)
   }
 })
