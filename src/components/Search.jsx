c76b043cb6351c3d9e25615d4a1082ff16877b0f
Andi <andicbuchanan@gmail.com>
2016-03-17 20:39:41 +1300

    search function formatted, welcome module working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ module.exports = React.createClass({
   render:function () {
     return (
       <div className="search">
-        <iframe src="https://duckduckgo.com/search.html?prefill=Search DuckDuckGo"frameborder="0"></iframe>
+        <iframe src="https://duckduckgo.com/search.html?prefill=Search DuckDuckGo" style={{margin: 0},{padding: 0},{width: 408},{height: 40}} frameborder="0"></iframe>
       </div>
     )
   }
