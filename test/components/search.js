2959ccc0aeacd30baa6287b8befb88b1b24832a7
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-17 21:21:45 +1300

    merge and add tests for search

---
+++ test/components/search.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+
+chai.use(chaiEnzyme())
+import { mount, render, shallow } from 'enzyme'
+import Search from '../../src/components/Search.jsx'
+
+describe('<Search />', () => {
+  it('has a div with class of search', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find('div.search')).to.have.length(1)
+  })
+  it('has an iframe with a duckduckgo search box', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains([<iframe src="https://duckduckgo.com/search.html?prefill=Search DuckDuckGo"frameborder="0"></iframe>])).to.equal(true)
+  })
+})
