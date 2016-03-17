353744b0a4975298e6694abacc82341949cc3b1c
Richard Joe <rjoe19@hotmail.com>
2016-03-17 14:56:48 +1300

    setup clock template hardcoded

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -5,46 +5,25 @@ import React from 'react'
 
 module.exports = React.createClass({
 
+  getInitialState: function() {
+     return {
+       time: "12.30",
+       am_pm: "AM"
+     };
+   },
+
   render: function () {
 
     return (
         <div>
           <h2>My Clock</h2>
-
-
+          <p>{this.state.time}</p>
+          <p>{this.state.am_pm}</p>
         <div>
 
+// set initial state...re displayTime mounted to clock
 
-      function displayTime() {
-            const currentTime = new Date();
-            const hours = currentTime.getHours();
-            const minutes = currentTime.getMinutes();
-            const seconds = currentTime.getSeconds();
-            const am = "AM";
-            if (seconds < 10) {
-                seconds = "0" + seconds
-            };
 
-            if (minutes < 10) {
-                minutes = "0" + minutes
-            };
-
-            if (hours < 10) {
-                hours = "0" + hours
-            };
-
-            if (hours > 12) {
-                hours = hours - 12;
-                am = "PM";
-            }
-
-            if (hours === 0) {
-                hours = 12;
-            }
-
-            const clockDiv = document.getElementById('clock');
-            setInterval(displayTime, 1000);
-            clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
         }
 
       }
